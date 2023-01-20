780d7f85ea0a46b29407467f09a8c2f890b772e4
awsmak <akash.47.de@gmail.com>
2023-01-20 14:19:08 +0100

    training, no  checkpoint

---
+++ test.py
@@ -0,0 +1,10 @@
+import os, time
+from operator import add
+import numpy as np
+from glob import glob
+import cv2
+from tqdm import tqdm
+import imageio
+import torch
+from sklearn.metrics import accuracy_score, f1_score, jaccard_score, precision_score
+
--- train.py
+++ train.py
@@ -10,6 +10,40 @@ from loss import DiceLoss, DiceBCELoss
 from utils import seeding, create_dir, epoch_time
 from data import DriveDataset
 
+
+def train(model, loader, optimizer, loss_fn, device):
+    epoch_loss = 0.0
+
+    model.train()
+    for x, y in loader:
+        x = x.to(device, dtype=torch.float32)
+        y = y.to(device, dtype=torch.float32)
+
+        optimizer.zero_grad()
+        y_pred = model(x)
+        loss = loss_fn(y_pred, y)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+    epoch_loss = epoch_loss / len(loader)
+    return epoch_loss
+
+
+def evaluate(model, loader, loss_fn, device):
+    epoch_loss = 0.0
+    model.eval()
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device, dtype=torch.float32)
+            y = y.to(device, dtype=torch.float32)
+
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+        epoch_loss += loss.item()
+    epoch_loss = epoch_loss / len(loader)
+    return epoch_loss
+
+
 if __name__ == "__main__":
     # Seeding
     seeding(42)
@@ -18,11 +52,11 @@ if __name__ == "__main__":
     create_dir("files")
 
     # Load dataset
-    train_x = glob("augmented_data/train/images/*")
-    train_y = glob("augmented_data/train/masks*")
+    train_x = sorted(glob("augmented_data/train/images/*"))
+    train_y = sorted(glob("augmented_data/train/masks/*"))
 
-    val_x = glob("augmented_data/test/images/*")
-    val_y = glob("augmented_data/test/masks*")
+    val_x = sorted(glob("augmented_data/test/images/*"))
+    val_y = sorted(glob("augmented_data/test/masks/*"))
 
     data_str = f"Dataset Size:\nTrain:{len(train_x)}, valid:{len(val_x)}"
     print(data_str)
@@ -65,5 +99,25 @@ if __name__ == "__main__":
     loss_fn = DiceBCELoss()
 
     """Training the model"""
-    
+    best_valid_loss = float("inf")
+    for epoch in range(num_epochs):
+        start_time = time.time()
 
+        train_loss = train(model, train_loader, optimizer, loss_fn, device)
+        valid_loss = evaluate(model, valid_loader, loss_fn, device)
+
+        """Saving the model"""
+        if valid_loss < best_valid_loss:
+            data_str = f"Validation loss improved from {best_valid_loss:2.4f} to {valid_loss:2.4f}. Saving checkpoint: {checkpoint_path}"
+            print(data_str)
+
+            best_valid_loss = valid_loss
+            torch.save(model.state_dict(), checkpoint_path)
+
+        end_time = time.time()
+        epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+        data_str = f"Epoch: {epoch + 1:02} | Epoch_time: {epoch_mins}m {epoch_secs}s\n"
+        data_str += f"\tTrain Loss: {train_loss: .3f}\n"
+        data_str += f"\tValidation loss: {valid_loss: .3f}\n"
+        print(data_str)
